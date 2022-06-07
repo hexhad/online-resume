06e1d756fd7a9c30c7e12e7ec32dfef16ba47cf9
hexhad <hexhad@gmail.com>
2022-06-07 14:50:33 -0700

    count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [value, setValue] = useState(1);
   return (
     <div className="App">
-HELLO
+{value}
+<button onClick={()=>{setValue(value+1)}}> click me </button>
+
     </div>
   );
 }
