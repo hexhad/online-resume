aaa3b1a001909a4ab1801daa890a03bdaf273357
hexhad <hexhad@gmail.com>
2022-06-07 15:05:13 -0700

    issue

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   const [value, setValue] = useState(1);
   return (
     <div className="App">
-{value}
-<button onClick={()=>{setValue(value+1)}}> click me </button>
-
+      <div className="value">{value}</div>
+      <button className="button"
+        onClick={() => {
+          setValue(value + 1);
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
